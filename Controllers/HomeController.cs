d91f5e220b5efd9b7d53d5a182e5cedb1e86b073
flippflopp <tremtos@gmail.com>
2021-09-18 11:47:11 +0300

    Created DB

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SigmaInternProject.Model;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SigmaInternProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private ApplicationContext db;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationContext context)
+        {
+            _logger = logger;
+            db = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
